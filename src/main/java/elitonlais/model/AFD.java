9ef1b883158cb65c749af2ce129bda908dc92890
Machado <elitonmahcadod200@gmail.com>
2020-08-23 17:49:08 -0300

    abandoned JLink, now MavenAssembly is my best friend

--- src/main/java/elitonlais/model/AFD.java
+++ src/main/java/elitonlais/model/AFD.java
@@ -1,6 +1,19 @@
 package elitonlais.model;
 
+import guru.nidi.graphviz.attribute.Label;
+import guru.nidi.graphviz.attribute.Shape;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.Graph;
+import guru.nidi.graphviz.model.Node;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
+
+import static guru.nidi.graphviz.model.Factory.*;
 
 public class AFD {
 
@@ -41,26 +54,26 @@ public class AFD {
         return grafo;
     }
 
-    //    public void geraPng() {
-//        Map<String, Node> nodes = new TreeMap<>(new StringComparator());
-//        for (String a : grafo.getNodes()) {
-//            if (estadosFinais.contains(a)) nodes.put(a, node(a).with(Label.nodeName(), Shape.DOUBLE_CIRCLE));
-//            else nodes.put(a, node(a).with(Label.nodeName(), Shape.CIRCLE));
-//        }
-//
-//        Graph g = graph("MdsLfa").directed();
-//        for (Pair<String, String> p : grafo.getEdges().keySet()) {
-//            for (String edge : grafo.getEdge(p)) {
-//                g = g.with(nodes.get(p.getFi()).link(to(nodes.get(p.getSe())).with(Label.of(edge))));
-//            }
-//        }
-//
-//        g = g.with(node("").with(Shape.POINT).link(to(nodes.get(estadoInicial))));
-//
-//        try {
-//            Graphviz.fromGraph(g).width(400).render(Format.PNG).toFile(new File("graph.png"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public void geraPng() {
+        Map<String, Node> nodes = new TreeMap<>();
+        for (String a : grafo.getNodes()) {
+            if (estadosFinais.contains(a)) nodes.put(a, node(a).with(Label.nodeName(), Shape.DOUBLE_CIRCLE));
+            else nodes.put(a, node(a).with(Label.nodeName(), Shape.CIRCLE));
+        }
+
+        Graph g = graph("MdsLfa").directed();
+        for (Pair<String, String> p : grafo.getEdges().keySet()) {
+            for (char c : grafo.getEdge(p)) {
+                g = g.with(nodes.get(p.getFi()).link(to(nodes.get(p.getSe())).with(Label.of(String.valueOf(c)))));
+            }
+        }
+
+        g = g.with(node("").with(Shape.POINT).link(to(nodes.get(estadoInicial))));
+
+        try {
+            Graphviz.fromGraph(g).width(400).render(Format.PNG).toFile(new File("graph.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
