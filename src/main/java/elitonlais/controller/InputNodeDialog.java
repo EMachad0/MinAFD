58cdeb98703deaca9c616dd91d8c9ce3b5d2324a
Machado <elitonmahcadod200@gmail.com>
2020-09-18 14:56:50 -0300

    Better Dialog Handling

--- src/main/java/elitonlais/controller/InputNodeDialog.java
+++ src/main/java/elitonlais/controller/InputNodeDialog.java
@@ -9,19 +9,17 @@ import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class InputArestaDialog implements Initializable {
+public class InputNodeDialog implements Initializable {
 
     private String dado = null;
 
     @FXML public Button btnOk;
     @FXML public TextField tf1;
-    @FXML public TextField tf2;
-    @FXML public TextField tf3;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         btnOk.setOnAction(evt -> {
-            dado = tf1.getText() + "," + tf2.getText() + "," + tf3.getText();
+            dado = tf1.getText();
             App.closeStage(evt);
         });
     }
